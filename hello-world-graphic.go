2994e1ae178f596472329c2459396732ecaba374
Mike Murphy <codermurph@sleepymurph.com>
2017-02-28 21:04:19 +0100

    Gut QML from hello-world so it will compile w/o QT

--- hello-world-graphic.go
+++ hello-world-graphic.go
@@ -5,46 +5,25 @@ import (
 	"math/rand"
         "os"
 	"time"
-
-	"gopkg.in/qml.v1"
 )
 
 func main() {
 
-	err := qml.Run(run)
+	err := run()
 	if err != nil {
 		log.Panic("Could not run QML man", err)
 	}
 }
 
 func run() error {
-	engine := qml.NewEngine()
-
-	component, err := engine.LoadFile("hello-world.qml")
-	if err != nil {
-		return err
-	}
 
         dir, _ := os.Getwd()
-	ctrl := Control{Message: dir}
 
-	context := engine.Context()
-	context.SetVar("ctrl", &ctrl)
-
-	window := component.CreateWindow(nil)
-
-	ctrl.Root = window.Root()
+	print (dir)
 
 	rand.Seed(time.Now().Unix())
 
-	window.Show()
 	time.Sleep(5000 * time.Millisecond)
 
 	return nil
-
-}
-
-type Control struct {
-	Root    qml.Object
-	Message string
 }
